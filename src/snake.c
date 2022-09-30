4da64cee3806df423c912321107bf81e1c05cbae
collinwu66 <92630234+collinwu66@users.noreply.github.com>
2022-09-30 15:59:08 +0800

    Update snake.c

--- src/snake.c
+++ src/snake.c
@@ -25,6 +25,8 @@
 #define   WALL      -2   //表示墙体
 #define   EMPTY      0   //表示没有物体的位置
 #define   HEAD       1   //表示蛇头
+#define   FOOD       -1  //表示食物
+ 
 
 //-------------------------------【全局结构体声明部分】----------------------------
 // 描述：声明一些结构体
@@ -49,18 +51,18 @@ typedef struct{
 // 描述：下面声明一个窗口过程（回调）函数。当事件发生时，Windows会系统调用该函数，
 //我们可以为我们感兴趣的事件写上处理代码。
 //--------------------------------------------------------------------------------
-LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc( HWND, UINT, WPARAM, LPARAM );
 
 //-------------------------------setNewTail()函数声明】----------------------------
 // 描述：找到新的蛇尾的位置，并通过第二个和第三个参数带回
 //--------------------------------------------------------------------------------
-void setNewTail(const int canvas[][50] ,int row,int* tail_x, int* tail_y);
+void setNewTail( const int canvas[][50] , int row, int* tail_x, int* tail_y );
 
 
 
 
 //定义一个WinMain函数，类似与c语言控制台程序要定义一个main函数来作为入口函数
-int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
                     PSTR szCmdLine, int iCmdShow)
 {
     /**************************************************************************/
@@ -69,7 +71,7 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
     HWND        hwnd;   //定义一个窗口句柄
     MSG         msg;    //定义一个消息结构
     WNDCLASS   wndclass;    //定义一个窗口类
-    static TCHAR AppName[] = TEXT("snake"); //给我们的窗口类取一个名字
+    static TCHAR AppName[]      = TEXT("snake"); //给我们的窗口类取一个名字
     //给窗口类各字段赋值
     wndclass.style              = CS_HREDRAW | CS_VREDRAW; //设置为窗口大小发生改变时要重新绘制
     wndclass.lpfnWndProc        = WndProc;  //为窗口类绑定窗口过程函数
@@ -82,9 +84,9 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
     wndclass.hIcon              = (HICON) LoadImage( NULL, TEXT("../ico/snake.ico"), IMAGE_ICON, 
                                                     0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
     
-    wndclass.hCursor            = LoadCursor (NULL, IDC_ARROW); //使用默认的鼠标指针
+    wndclass.hCursor            = LoadCursor( NULL, IDC_ARROW ); //使用默认的鼠标指针
     //选择一个喜欢颜色的画刷，系统会用这种颜色把我们的窗口背景刷成黑色
-    wndclass.hbrBackground      = (HBRUSH) GetStockObject (BLACK_BRUSH);    
+    wndclass.hbrBackground      = (HBRUSH) GetStockObject( BLACK_BRUSH );    
     wndclass.lpszClassName      = AppName;  //前面已经取好了的名字
     /*****************************************************************************************/
 
@@ -92,16 +94,16 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
     /*******************************************************************************/
     //Step2:注册窗口类，告诉Windows我们的窗口属性已经设定好了
     /*******************************************************************************/
-    if ( !RegisterClass (&wndclass) )
+    if ( !RegisterClass(&wndclass) )
     {
         // 如果注册失败，输出提示信息
-        MessageBox (NULL, TEXT("error!!!"), TEXT("snake"), MB_ICONERROR);
+        MessageBox( NULL, TEXT("error!!!"), TEXT("snake"), MB_ICONERROR );
 
         return 0;
     }
 
     //创建窗口
-    hwnd = CreateWindow ( AppName,               //窗口类名称
+    hwnd = CreateWindow( AppName,               //窗口类名称
                           TEXT("snake"),         //窗口的标题
                           WS_OVERLAPPEDWINDOW,   //默认这样就好了
                           CW_USEDEFAULT,         //窗口出现的x坐标,设为默认的好了
@@ -113,14 +115,14 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
                           hInstance,
                           NULL);
 
-    ShowWindow (hwnd, iCmdShow);//显示窗口，这个时候实实在在可以看到我们的窗口了
+    ShowWindow( hwnd, iCmdShow );//显示窗口，这个时候实实在在可以看到我们的窗口了
     //接下来显示内容
-    UpdateWindow (hwnd);
+    UpdateWindow ( hwnd );
     //消息循环
-    while (GetMessage (&msg, NULL, 0 ,0))
+    while ( GetMessage( &msg, NULL, 0 ,0 ) )
     {
-        TranslateMessage (&msg);
-        DispatchMessage (&msg);
+        TranslateMessage( &msg );
+        DispatchMessage( &msg );
     } 
 
     return msg.wParam;
@@ -132,7 +134,7 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
 //step3:在窗口过程中写游戏主体代码
 /*******************************************************************************/
 
-LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc( HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam )
 {
     HDC                     hdc;//定义一个设备dc，方便绘图
     PAINTSTRUCT              ps;
@@ -143,19 +145,19 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
     //     0  ------------  代表无元素----------  EMPTY
     //     1  ------------  代表蛇头  ----------  HEAD
     //     2-n -----------  代表蛇身  
-    static int s_canvas[60][50] = {EMPTY}; 
+    static int s_canvas[60][50] = { EMPTY }; 
     static SNAKE s_snake;
-    int row,col;
+    int row, col;
     HPEN            pen;
     HBRUSH          hBrush;
     static  FOOD    food;
-    switch (message)
+    switch ( message )
     {
         /***********************************************/
         //step4: 在WM_CREATE中进行游戏的初始化
         /************************************************/
         case WM_CREATE:
-            GetClientRect(hwnd,&rect);
+            GetClientRect( hwnd, &rect );
             //初始化墙体
             for (row=0; row<60; row++)
             {
@@ -181,7 +183,7 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
             food.x =  45;
             food.y = 45;
             s_canvas[food.x][food.y] = -1;
-            SetTimer(hwnd,1,300,NULL);//由于机制问题，这种定时方式不准，简单起见，将就着用
+            SetTimer( hwnd, 1, 300, NULL );//由于机制问题，这种定时方式不准，简单起见，将就着用
             break;
 
         /*********************************************************************/
@@ -192,7 +194,7 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
             { 
                 for (row=0; row < 60; row++) 
                 {
-                    if (s_canvas[row][col] > 0)
+                    if ( s_canvas[row][col] > 0 )
                     {
                         s_canvas[row][col] = s_canvas[row][col] + 1;
                     }
@@ -200,10 +202,10 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
                 }
             }
             //边界检查
-            switch(s_snake.direction)
+            switch( s_snake.direction )
             {
                 case 'R':
-                    if ( !(s_snake.head_x == 58) && !(s_canvas[s_snake.head_x+1][s_snake.head_y] > 0))//向右没撞墙也没有撞到自己
+                    if ( !(s_snake.head_x == 58) && !(s_canvas[s_snake.head_x+1][s_snake.head_y] > 0) ) //向右没撞墙也没有撞到自己
                     {
                         s_canvas[++s_snake.head_x][s_snake.head_y] = 1;//新蛇头
                     }
@@ -263,48 +265,48 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
             //间接发送WM_PAINT消息
             //第三个参数为FALSE表示重绘时不擦除原来显示的内容，可以防止画面闪烁
-            InvalidateRect(hwnd,NULL,TRUE);
+            InvalidateRect( hwnd, NULL, TRUE);
             break;
 
         //step6:在此处进行游戏界面的绘制
         case WM_PAINT://在该消息中根据s_canvas的内容进行界面的绘制
-            hdc = BeginPaint (hwnd, &ps);//绘图从此处开始
-            pen = CreatePen(PS_INSIDEFRAME,1,RGB(255,0,0));//创建红色画笔
-            SelectObject (hdc,pen);//将画笔选入当前设备
+            hdc = BeginPaint( hwnd, &ps );//绘图从此处开始
+            pen = CreatePen( PS_INSIDEFRAME, 1, RGB(255, 0, 0) );//创建红色画笔
+            SelectObject( hdc, pen );//将画笔选入当前设备
             //绘制界面
-            hBrush = CreateSolidBrush(RGB(0,255,0));//创建绿色画刷
-            SelectObject(hdc,hBrush);//将画刷选入当前设备
+            hBrush = CreateSolidBrush( RGB(0, 255, 0) );//创建绿色画刷
+            SelectObject( hdc, hBrush );//将画刷选入当前设备
             for (col=0; col < 50; col++)
             {
                 for (row=0; row < 60; row++)   
                 {
                     //绘制蛇身
-                    if (s_canvas[row][col] > 1)
+                    if (s_canvas[row][col] > HEAD)
                     {
-                        Rectangle(hdc,row*10,col*10,row*10+10,col*10+10);
+                        Rectangle( hdc, row * 10, col * 10, row * 10 + 10, col * 10 + 10 );
                     }
                     //绘制蛇头
-                    if (s_canvas[row][col] == 1)
+                    if (s_canvas[row][col] == HEAD)
                     {
-                        Ellipse(hdc,row*10,col*10,row*10+10,col*10+10);
+                        Ellipse( hdc, row * 10, col * 10, row * 10 + 10, col * 10 + 10 );
                     }
                     //绘制食物
-                    if (s_canvas[row][col] == -1 )
+                    if ( s_canvas[row][col] == FOOD )
                     {
-                        Ellipse(hdc,row*10,col*10,row*10+10,col*10+10);
+                        Ellipse( hdc, row * 10, col * 10, row * 10 + 10, col * 10 + 10);
                     }
                     //绘制墙体
-                    if (s_canvas[row][col] == -2)
+                    if ( s_canvas[row][col] == WALL)
                     {
-                        Rectangle(hdc,row*10,col*10,row*10+10,col*10+10);
+                        Rectangle( hdc, row * 10, col * 10, row * 10 + 10, col * 10 + 10 );
                     }    
                 }
             }
-            EndPaint (hwnd, &ps);//绘图结束
+            EndPaint( hwnd, &ps );//绘图结束
             break;
 
         case WM_KEYDOWN://在键盘按下消息中处理蛇前进方向的设置
-            switch(wParam)
+            switch( wParam )
             {
                 case  'W'://此处的大写W只代表键盘上的字母键W被按下，与你输入时的大小写状态无关
                 case VK_UP://设置蛇的运动方向为向上
@@ -344,15 +346,15 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 
         case WM_DESTROY://在此处进行善后工作
 
-            PostQuitMessage(0);//保证程序彻底退出
+            PostQuitMessage( 0 );//保证程序彻底退出
             break;
         
     }
-    return DefWindowProc (hwnd, message, wParam, lParam);
+    return DefWindowProc( hwnd, message, wParam, lParam );
 }
 
 //找到新蛇尾的位置
-void setNewTail(const int canvas[][50] , int row, int* tail_x, int* tail_y)
+void setNewTail( const int canvas[][50] , int row, int* tail_x, int* tail_y )
 {
     int r,col;
     for (r=0; r < row; r++)
